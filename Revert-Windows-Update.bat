8e3972f55909d7511c31634b6f247000beb4bbb3
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-09-07 16:11:56 +0400

    Add files via upload

---
+++ Revert-Windows-Update.bat
@@ -0,0 +1,4 @@
+net stop wuauserv
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v DisableWindowsUpdateAccess /d 1 /t REG_DWORD /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoRebootWithLoggedOnUsers /d 1 /t REG_DWORD /f
+net start wuauserv
