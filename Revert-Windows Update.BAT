a6a3d5e51fedd8479855512ebb59ae57ff898ce9
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-09-07 16:10:03 +0400

    Delete Revert-Windows Update.BAT

--- Revert-Windows Update.BAT
+++
@@ -1,4 +0,0 @@
-net stop wuauserv
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v DisableWindowsUpdateAccess /d 1 /t REG_DWORD /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoRebootWithLoggedOnUsers /d 1 /t REG_DWORD /f
-net start wuauserv
